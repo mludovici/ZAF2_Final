88263a239a39be6ed0161d9c06923a51e030fa9d
Ludovici <ludovicimarc@gmail.com>
2023-02-28 13:01:24 +0100

    Main Controller bereinigt

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -9,48 +9,7 @@ sap.ui.define([
 
         return Controller.extend("zaf2final.controller.Main", {
             onInit: function () {
-                var oModel = new JSONModel();
-                oModel.loadData("/data/asdasd", {}, false);
-
-                oModel2.setData({
-                    "movie": { "TEST"},
-                    "artist": { "TEST ÄNDERUNG"}
-                });
-                this.getView().setModel(oModel2, "asaasdasd");
-            },
-
-
-            onExpandFirstLevel: function () {
-                var oTreeTable = this.byId("TreeTableBasic");
 
             },
-
-            onExpandSelection: function () {
-                var oTreeTable = this.byId("TreeTableBasic");
-                oTreeTable.expand(oTreeTable.getSelectedIndices());
-            },
-
-            onRowSelectionChange: function () {
-                debugger;
-                //TEST
-                //Iasdasdasd("splitapp01");
-                var oTable = this.getView().byId("TreeTableBasic");
-
-                var oModelDet = this.getView().getModel("detailMod");
-                //Get the selected row
-                var iSelRowId = oTable.getSelectedIndex();
-                //Find the movie Object and set the "Single Movie" Object
-                var sMovModPath = oTable.getContextByIndex(iSelRowId).getPath(); //"/artRoot/0/artMovies/1"
-                var oMovie = oModel.getObject(sMovModPath);
-                oModelDet.setProperty("/movie", oMovie);
-                //Determine the artist path and set the "Single Movie" Object
-                var iIdx = sMovModPath.indexOf("/artMovies");
-                var sMovArtPath = "";
-
-                var oArtist = oModel.getObject(sMovArtPath);
-                oModelDet.setProperty("/artist", oArtist);
-                //Call the detail page
-                oSplitApp.toDetail(this.createId("DetailPage"));
-            }
         });
     });
